982c645e95d32ae8780c69ea564f9859745e88bb
Alexandru Neculaes <alexandru.neculaes@gmail.com>
2022-07-29 20:45:53 +0100

    first deploy

---
+++ client/build/precache-manifest.7c37b99f28b1865b238b8f8a710f0392.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5183ce1c72c012ce283ed4ca4b94957a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b0041d7374631d3ab4a",
+    "url": "/static/css/main.68f6ff43.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b0041d7374631d3ab4a",
+    "url": "/static/js/main.bf40b51c.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "c2d61c546a8dd6d0bc6dcec2d21f3301",
+    "url": "/static/media/BookTalk.c2d61c54.png"
+  }
+]);
